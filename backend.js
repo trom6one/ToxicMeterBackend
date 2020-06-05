25755312537cb0a7b031103db5e189eae48956da
trom6one <trom6one@gmail.com>
2020-06-05 18:47:58 +1100

    Update backend.js

--- backend.js
+++ backend.js
@@ -144,10 +144,6 @@ app.post('/fill/amount', function(req, res) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
-})
-
-
-app.on('listening', function () {
 
   fs.readFile(__dirname + '/channelsData.json', (err, data) => {
     if (err) throw err;
