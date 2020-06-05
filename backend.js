f49e0e5861e1826b1d21fe6d41f90336b478ab19
trom6one <trom6one@gmail.com>
2020-06-05 19:15:02 +1100

    Update backend.js

--- backend.js
+++ backend.js
@@ -152,15 +152,15 @@ app.listen(app.get('port'), function() {
   fs.readFileSync(__dirname + '/channelsData.json', (err, data) => {
     if (err) throw err;
     let readedJson = JSON.parse(data);
-    console.log(`readedJson['channelAmounts'] = ${JSON.stringify(readedJson['channelAmounts'])}`);
-    channelCooldowns = JSON.stringify(readedJson['channelCooldowns']);
-    channelAmounts = JSON.stringify(readedJson['channelAmounts']);
-    vievewsCounts = JSON.stringify(readedJson['vievewsCounts']);
-    userCooldowns = JSON.stringify(readedJson['userCooldowns']);
-    decreaseTimer = JSON.stringify(readedJson['decreaseTimer']);
-    decreaseTimerActive = JSON.stringify(readedJson['decreaseTimerActive']);
+    console.log(`readedJson['channelAmounts'] = ${JSON.stringify(readedJson.channelAmounts)}`);
+    // channelCooldowns = JSON.stringify(readedJson['channelCooldowns']);
+    // channelAmounts = JSON.stringify(readedJson['channelAmounts']);
+    // vievewsCounts = JSON.stringify(readedJson['vievewsCounts']);
+    // userCooldowns = JSON.stringify(readedJson['userCooldowns']);
+    // decreaseTimer = JSON.stringify(readedJson['decreaseTimer']);
+    // decreaseTimerActive = JSON.stringify(readedJson['decreaseTimerActive']);
   });
-  
+
   initDataSaveTimer(10000);
 });
 
